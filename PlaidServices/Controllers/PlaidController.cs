86d2e6933f03d3fe4766e4ef4b8ac85ff2f2560c
aeaov <andrew.overton@american-equity.com>
2025-02-12 14:02:35 -0600

    initial commit

---
+++ PlaidServices/Controllers/PlaidController.cs
@@ -0,0 +1,57 @@
+﻿// Controllers/PlaidController.cs
+using Microsoft.AspNetCore.Mvc;
+using PlaidIntegration.Services;
+using System.Threading.Tasks;
+
+namespace PlaidIntegration.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlaidController : ControllerBase
+    {
+        private readonly PlaidService _plaidService;
+
+        public PlaidController(PlaidService plaidService)
+        {
+            _plaidService = plaidService;
+        }
+
+        // Step 1: Create a link token
+        [HttpGet("create_link_token")]
+        public async Task<IActionResult> CreateLinkToken()
+        {
+            var linkToken = await _plaidService.CreateLinkTokenAsync("user_good");
+            return Ok(new { link_token = linkToken });
+        }
+
+        // Step 2: Exchange public token for access token
+        [HttpPost("exchange_public_token")]
+        public async Task<IActionResult> ExchangePublicToken([FromBody] ExchangePublicTokenRequest request)
+        {
+            var accessToken = await _plaidService.ExchangePublicTokenAsync(request.PublicToken);
+            return Ok(new { access_token = accessToken });
+        }
+
+        // Step 3: Get account info (routing and account number)
+        [HttpPost("get_account_info")]
+        public async Task<IActionResult> GetAccountInfo([FromBody] GetAccountInfoRequest request)
+        {
+            var accountInfo = await _plaidService.GetAccountInfoAsync(request.AccessToken);
+            return Ok(new
+            {
+                account_number = accountInfo.Name,
+                routing_number = accountInfo.Balances
+            });
+        }
+    }
+
+    public class ExchangePublicTokenRequest
+    {
+        public string PublicToken { get; set; }
+    }
+
+    public class GetAccountInfoRequest
+    {
+        public string AccessToken { get; set; }
+    }
+}
---
+++ PlaidServices/Services/PlaidService.cs
@@ -0,0 +1,65 @@
+﻿// Services/PlaidService.cs
+using Microsoft.Extensions.Configuration;
+using PlaidIntegration.Controllers;
+using Going.Plaid;
+using System.Threading.Tasks;
+using Going.Plaid.Link;
+using Going.Plaid.Entity;
+
+namespace PlaidIntegration.Services
+{
+    public class PlaidService
+    {
+        private readonly PlaidClient _plaidClient;
+        private readonly string _clientId;
+        private readonly string _secret;
+
+        public PlaidService(IConfiguration configuration)
+        {
+            _plaidClient = new PlaidClient(
+                Going.Plaid.Environment.Sandbox,
+                secret: "80f488dc08e104e63cd156c8d7f55a",
+                clientId: "67acd56f70dce6002146c70a");
+
+        }
+
+        public async Task<string> CreateLinkTokenAsync(string clientUserId)
+        {
+
+            var linkTokenRequest = new LinkTokenCreateRequest
+            {
+                User = new LinkTokenCreateRequestUser {ClientUserId = "user_good"},
+                ClientName = "Sandbox",
+                Products = new List<Products> { Products.Auth },
+                CountryCodes = new List<CountryCode> { CountryCode.Us },
+                Language = Language.English
+            };
+
+
+            var linkTokenResponse = await _plaidClient.LinkTokenCreateAsync(linkTokenRequest);
+            return linkTokenResponse.LinkToken;
+        }
+
+        public async Task<string> ExchangePublicTokenAsync(string publicToken)
+        {
+            var exchangeRequest = new Going.Plaid.Item.ItemPublicTokenExchangeRequest
+            {
+                PublicToken = publicToken
+            };
+
+            var exchangeResponse = await _plaidClient.ItemPublicTokenExchangeAsync(exchangeRequest);
+            return exchangeResponse.AccessToken;
+        }
+
+        public async Task<Going.Plaid.Entity.Account> GetAccountInfoAsync(string accessToken)
+        {
+            var authResponse = await _plaidClient.AccountsGetAsync(new Going.Plaid.Accounts.AccountsGetRequest
+            {
+                AccessToken = accessToken
+            });
+
+            // Returning the first account's routing number and account number
+            return authResponse.Accounts[0];
+        }
+    }
+}
